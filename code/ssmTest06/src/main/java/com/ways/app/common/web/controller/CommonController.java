ff8527691332a780446dd76ab2e60109e438651e
haiyong6 <1025802940@qq.com>
2019-07-19 18:10:01 +0800

    iii

--- code/ssmTest06/src/main/java/com/ways/app/common/web/controller/CommonController.java
+++ code/ssmTest06/src/main/java/com/ways/app/common/web/controller/CommonController.java
@@ -1,5 +1,8 @@
 package com.ways.app.common.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,4 +14,14 @@ public class CommonController {
         return "test";
     }
 
+    @RequestMapping("/commonController/logOut")
+    public void logOut(HttpSession requestSession) {
+        try {
+            requestSession.invalidate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+       // return "index";
+    }
+    
 }
