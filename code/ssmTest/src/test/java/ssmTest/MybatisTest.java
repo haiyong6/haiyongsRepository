be79a96c08f00aa08451cecbdba6d2b9443fb009
haiyong6 <1025802940@qq.com>
2019-07-21 11:37:08 +0800

    重新修改ssmTest

--- code/ssmTest/src/test/java/ssmTest/MybatisTest.java
+++ code/ssmTest/src/test/java/ssmTest/MybatisTest.java
@@ -3,18 +3,15 @@ package ssmTest;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.logging.Log;
-import org.apache.ibatis.logging.LogFactory;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
-import com.ways.app.common.dao.CommonMapper;
+import com.ways.app.dao.UserMapper;
 
 public class MybatisTest {
     @Test
@@ -32,20 +29,21 @@ public class MybatisTest {
         SqlSession sqlSession = factory.openSession();
         
         //4.获取mapper接口的对象
-        CommonMapper commonMapper = sqlSession.getMapper(CommonMapper.class);
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
         
         //操作
         Map<String, Object> paramsMap = new HashMap<String, Object>();
-        List<Map<String, Object>> userList = commonMapper.getUserList(paramsMap);
-        for(int i = 0; i < userList.size(); i++) {
-            Map<String, Object> userMap = userList.get(i);
-            System.out.println("用户名: " + userMap.get("USER_NAME"));
-        }
+        paramsMap.put("userName", "zhaohy");
+        paramsMap.put("sex", 1);
+        paramsMap.put("job", "java软件工程师");
+        paramsMap.put("tel", "189xxxx0598");
+        paramsMap.put("email", "1025XXXX40@qq.com");
+        paramsMap.put("hobby", "编程，运动");
+        userMapper.saveUser(paramsMap);
         //6.提交事务
         sqlSession.commit();
         
         //7.关闭资源
         sqlSession.close();
-        
     }
 }
